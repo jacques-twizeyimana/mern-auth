3c34bdedd51054d47910d4d6db3c4f1a8d8ac6a4
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-04-12 14:36:17 +0200

    feat: authentication  middleware

---
+++ server/utils/middleware/auth.middleware.js
@@ -0,0 +1,38 @@
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const { createError } = require("../htpp-response");
+
+function isAdmin(req, res, next) {
+  if (req.user.isAdmin === false)
+    return res
+      .status(403)
+      .send("Access Denied for this user.This is for authorized admins only.");
+  else next();
+}
+
+function isAuthenticated(req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token)
+    return res
+      .status(200)
+      .send(
+        createError(
+          401,
+          "This service is for authorised users only.plz got to login to access it"
+        )
+      );
+
+  try {
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    return res
+      .status(200)
+      .send(
+        createError(400, "Your session data was destroyed. go to login again")
+      );
+  }
+}
+
+module.exports = { isAdmin, isAuthenticated };
