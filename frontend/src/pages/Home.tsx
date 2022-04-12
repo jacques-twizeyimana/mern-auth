e4adc6163d6916875253f7ecdfc8d7629c500231
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-04-12 11:24:44 +0200

    feat: public home text frontend functionality

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
-  const [isEditing, setisEditing] = useState(true);
+  const [draftText, setDraftText] = useState("");
+  const [publicText, setPublicText] = useState("");
+  const [isEditing, setisEditing] = useState(false);
+
+  useEffect(() => {
+    setDraftText(publicText);
+  }, [publicText]);
+
+  useEffect(() => {
+    setPublicText(`Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+      voluptatum quisquam voluptatibus expedita excepturi autem aperiam minus
+      deserunt laborum illum. Velit deserunt rerum, dolorum hic autem nobis
+      odit? Accusamus, et?`);
+  }, []);
+
+  const handleSave = () => {
+    setisEditing(false);
+    setPublicText(draftText);
+  };
+
   return (
     <div className="lg:w-2/3 mx-auto py-14 lg:py-20 xl:py-24 px-6">
-      <div className="text-right">
+      <div className={isEditing ? "hidden" : "text-right"}>
         <button
           className="px-2 py-2 bg-gray-50 rounded-full"
           onClick={() => setisEditing(true)}
@@ -23,12 +42,36 @@ export default function Home() {
           </svg>
         </button>
       </div>
-      <p className="text-2xl lg:text-3xl italic text-gray-500 tracking-wide">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-        voluptatum quisquam voluptatibus expedita excepturi autem aperiam minus
-        deserunt laborum illum. Velit deserunt rerum, dolorum hic autem nobis
-        odit? Accusamus, et?
+      <p
+        className={`${
+          isEditing ? "hidden" : "block"
+        } text-2xl lg:text-3xl italic text-gray-500 tracking-wide`}
+      >
+        {publicText}
       </p>
+      <div className={`${isEditing ? "block" : "hidden"}`}>
+        <textarea
+          name="text"
+          className="border-2 border-gray-800 focus:outline-none rounded w-full h-64 p-4 mb-4 resize-none"
+          value={draftText}
+          onChange={(e) => setDraftText(e.target.value)}
+        />
+        <div className="my-2 flex gap-4">
+          <button
+            className="border-2  border-gray-800 px-8 rounded py-2 text-center"
+            onClick={() => setisEditing(false)}
+          >
+            Discard
+          </button>
+
+          <button
+            className="border-2 rounded bg-gray-800 px-10 text-white py-2 text-center"
+            onClick={handleSave}
+          >
+            Save
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
