8acde1b56efb2c245bddbb11176d94d736e2ffe6
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-04-13 11:56:55 +0200

    feat: public text controller

---
+++ server/controller/text.controller.js
@@ -0,0 +1,39 @@
+const Joi = require("joi");
+const express = require("express");
+
+const Text = require("../model/text.model");
+const { createError, createSuccess } = require("../utils/htpp-response");
+
+var router = express.Router();
+
+router.get("/", async (req, res) => {
+  let text = await Text.findOne();
+
+  if (!text) {
+    text = new Text({});
+    await text.save();
+  }
+
+  return res.send(createSuccess(text));
+});
+
+router.put("/", async (req, res) => {
+  const { error } = validateText(req.body);
+  if (error) return res.send(createError(400, error.details[0].message));
+
+  Text.findOneAndUpdate(null, req.body, { new: true })
+    .then((text) => res.send(createSuccess(text)))
+    .catch((err) =>
+      res.send(createError(404, err.message || "Internal server error"))
+    );
+});
+
+function validateText(body) {
+  const schema = Joi.object({
+    content: Joi.string().max(255).min(3).required(),
+    lastUpdatedBy: Joi.string().max(255).min(3),
+  });
+  return schema.validate(body);
+}
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -12,12 +12,13 @@ require("./model/db");
 // controllers
 
 const userController = require("./controller/user.controller");
-
 const authController = require("./controller/auth.controller");
+const textController = require("./controller/text.controller");
 
 app.get("/", (req, res) => res.send(config.get("APP_DETAILS")));
 app.use("/api/v1/users", userController);
 app.use("/api/v1/auth", authController);
+app.use("/api/v1/text", textController);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`App listening on port ${port}!`));
