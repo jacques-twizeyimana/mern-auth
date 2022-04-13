b57152130bb1f09506e2a34a6a1f187462116938
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-04-13 11:57:28 +0200

    chore(user): use mongoose timestamps

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -1,42 +1,40 @@
 const mongoose = require("mongoose");
 
-var userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    maxlength: 255,
-    minlength: 3,
+var userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      maxlength: 255,
+      minlength: 3,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      maxlength: 255,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      maxlength: 255,
+      minlength: 3,
+    },
+    password: {
+      type: String,
+      required: true,
+      maxlength: 1024,
+      minlength: 3,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-    maxlength: 255,
-    minlength: 3,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    maxlength: 255,
-    minlength: 3,
-  },
-  password: {
-    type: String,
-    required: true,
-    maxlength: 1024,
-    minlength: 3,
-  },
-  isAdmin: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
