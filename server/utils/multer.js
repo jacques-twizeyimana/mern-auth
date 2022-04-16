e82fba03b0af01f223d10bf2a4965239a2b4d770
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-04-16 18:11:00 +0200

    feat: multer upload configuration

---
+++ server/utils/multer.js
@@ -0,0 +1,19 @@
+var multer = require("multer");
+const { makeId } = require("./random");
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/uploads/rooms");
+  },
+  filename: function (req, file, cb) {
+    let fileExtension = file.originalname.substring(
+      file.originalname.lastIndexOf("."),
+      file.originalname.length
+    );
+    cb(null, file.originalname + makeId(10) + fileExtension);
+  },
+});
+
+var upload = multer({ storage: storage });
+
+module.exports = upload;
